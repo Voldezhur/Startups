a3f4eb15ec12f3fee29f80fa2a9d91c69fe7e2a1
DQD1K <dreadgenius110@gmail.com>
2025-04-08 19:00:51 +0300

    Добавлена озвучка на серв
    Серв ждет на вход txt и возвращает озвученный wav

--- RAL_backend/main.go
+++ RAL_backend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
+	//"fmt"
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
+
+	// "path/filepath"
 	"strings"
 	"time"
 )
@@ -59,9 +64,67 @@ func mp3test(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func version_alpha(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Метод не разрешен", http.StatusMethodNotAllowed)
+		return
+	}
+
+	r.ParseMultipartForm(10 << 20) // 10 MB limit
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Не удалось получить файл", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Ошибка чтения файла", http.StatusInternalServerError)
+		return
+	}
+
+	// Выполняем C#-приложение
+	cmd := exec.Command("C:/Egorka/Startups/RAL_backend/ozvuchka/readaloud.exe", string(content))
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		http.Error(w, "Ошибка выполнения приложения: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Записываем полученные данные в output3.wav
+	err = writeWavFile("output3.wav", out.Bytes())
+	if err != nil {
+		http.Error(w, "Ошибка записи файла: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Отправляем сгенерированный файл
+	fileToSend, err := os.Open("debug_output.wav")
+	if err != nil {
+		http.Error(w, "Файл не найден", http.StatusNotFound)
+		return
+	}
+	defer fileToSend.Close()
+
+	w.Header().Set("Content-Type", "audio/wav")
+	w.Header().Set("Content-Disposition", "attachment; filename=file.wav")
+	w.WriteHeader(http.StatusOK)
+
+	http.ServeContent(w, r, "debug_output.wav", time.Now(), fileToSend)
+}
+
+func writeWavFile(filename string, data []byte) error {
+	return os.WriteFile(filename, data, 0644)
+}
+
 func main() {
 	http.HandleFunc("/uppercase", uppercaseHandler)
 	http.HandleFunc("/mp3_test", mp3test)
+	http.HandleFunc("/test", version_alpha)
 
 	http.ListenAndServe(":8080", nil)
 }
